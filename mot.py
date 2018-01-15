219e79dfc0a98b62d4a3ef05d11ed5ad09d55597
Robbie <txt3rob@gmail.com>
2018-01-15 11:45:49 +0000

    Update mot.py

--- mot.py
+++ mot.py
@@ -30,24 +30,33 @@ def grab_details (REG):
 		print ("Reg: "+REG+"")
 		print ("Color:"+vcol+"")
 		print ("Make:"+vmod+"")
-
-		# Submit to 2nd page to grab the details of the MOT and Tax
-		paramsPost2 = {"Vrm":REG,"Make":vmod,"viewstate":viewstate,"Continue":"","Correct":"True","Colour":vcol}
-		headers2 = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Referer":"https://vehicleenquiry.service.gov.uk/ConfirmVehicle","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/x-www-form-urlencoded"}
-		response2 = session.post("https://vehicleenquiry.service.gov.uk/ViewVehicle", data=paramsPost2, headers=headers2)
-	
-		# Regex to find the  Tax and MOT details.
-		if "No MOT</h2>" in response2.text:
-			print "This Vehicle has no MOT and current has a SORN registered for it."
-			exit();
-		
-		tax = re.compile('<strong>Tax due:<br>(.+?)</strong></p>').findall(response2.text)[0]
-		mot = re.compile('<strong>Expires:<br>(.+?)</strong></p>').findall(response2.text)[0]
-		print "Road Tax Expires: "+tax+""
-		print "Mot Expires: "+mot+"" 
 	except:
 		print "Sorry it looks like that registration has not been found"
 		print "Please try another registration."
+		
+
+	# Submit to 2nd page to grab the details of the MOT and Tax
+	paramsPost2 = {"Vrm":REG,"Make":vmod,"viewstate":viewstate,"Continue":"","Correct":"True","Colour":vcol}
+	headers2 = {"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Upgrade-Insecure-Requests":"1","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0","Referer":"https://vehicleenquiry.service.gov.uk/ConfirmVehicle","Connection":"close","Accept-Language":"en-US,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/x-www-form-urlencoded"}
+	response2 = session.post("https://vehicleenquiry.service.gov.uk/ViewVehicle", data=paramsPost2, headers=headers2)
+	
+	# Regex to find the  Tax and MOT details.
+	if "No MOT</h2>" in response2.text:
+		print "This Vehicle has no MOT and current has a SORN registered for it."
+		exit();
+	if "No details held by DVLA" in response2.text:
+		
+		print "Road Tax Expires: "+tax+""
+		print "MOT: No details held by DVLA"
+		exit();
+		
+	
+	tax = re.compile('<strong>Tax due:<br>(.+?)</strong></p>').findall(response2.text)[0]
+	mot = re.compile('<strong>Expires:<br>(.+?)</strong></p>').findall(response2.text)[0]
+	print "Road Tax Expires: "+tax+""
+	print "Mot Expires: "+mot+"" 
+	
+	
 
 
 # Instantiate the parser
